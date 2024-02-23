c3207192831fd646dcfa7e752c232e993c377455
Durgesh <durgeshsonarjee@gmail.com>
2024-02-23 13:47:29 +0530

    changed

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -16,7 +16,7 @@ const HomePage = () => {
             toast.error('Enter valid email!');
         else if(Email.indexOf('.') === -1)
             toast.error('Enter valid email!');
-        else if(Password !== 'CF@2024')
+        else if(Password !== 'CF--24')
             toast.error('Enter Valid Password!');
         else{
             setLoading(true);
